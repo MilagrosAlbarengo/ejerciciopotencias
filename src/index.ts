f574ea6ce0bbb5f7ddb0e53097516a0b9053a3ad
MilagrosAlbarengo <92064009+MilagrosAlbarengo@users.noreply.github.com>
2021-10-25 21:57:21 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+let base: number = document.getElementById("base");
+let exponente: number = document.getElementById("exponente");
+let btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+function calcular(base:number , exponente:number) {
+  for(let i;number = i=3 ; i*i*i*i){
+    console.log(exponente (i,i*i));
+  }
+  
+}
+}
